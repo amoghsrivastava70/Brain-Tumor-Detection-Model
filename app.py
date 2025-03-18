9927bb32254bf4416d76510a619251d7df632ec0
Amogh Srivastava <amoghsrivastava7080@gmail.com>
2025-03-18 21:26:18 +0530

    Added

---
+++ app.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import streamlit as st
+import numpy as np
+import cv2
+from PIL import Image
+
+# Load the TFLite model
+interpreter = tf.lite.Interpreter(model_path="Brain_T_Model.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+class_labels = ["Glioma", "Meningioma", "No Tumor", "Pituitary Tumor"]
+
+def preprocess_image(image):
+    """ Preprocess the uploaded image to match TFLite model input requirements. """
+    image = np.array(image)
+    image = cv2.resize(image, (input_details[0]['shape'][1], input_details[0]['shape'][2]))  
+    image = image / 255.0  
+    image = np.expand_dims(image, axis=0).astype(np.float32)  
+    return image
+
+# Streamlit UI
+st.title("Brain Tumor Classification")
+
+uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    # Preprocess image
+    processed_image = preprocess_image(image)
+
+    # Run inference using TFLite
+    interpreter.set_tensor(input_details[0]['index'], processed_image)
+    interpreter.invoke()
+    predictions = interpreter.get_tensor(output_details[0]['index'])
+
+    # Get predicted class
+    predicted_class = np.argmax(predictions, axis=1)[0]
+
+    # Display result
+    st.write(f"**Predicted Class:** {class_labels[predicted_class]}")
